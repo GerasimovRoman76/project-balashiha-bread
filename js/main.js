e310cb785916373ba9f8168fbbc0ad7fc600db52
Роман <gerasimov.roman.v@mail.ru>
2020-01-17 20:34:01 +0300

    feat: add section fotos in index.html and styles

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+});
